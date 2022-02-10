2ddd997898589736960a4f2502a8b11d7bda71dc
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-02-10 14:10:15 +0000

    Added next and previous links to posts page

--- public/src/myface/src/Components/Posts/postListPage/PostListPage.jsx
+++ public/src/myface/src/Components/Posts/postListPage/PostListPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { PostList } from "../postList/PostList";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 
 export function PostListPage() {
     const[postList, setPostList] = useState();
+    const[next, setNext] = useState();
+    const[previous, setPrevious] = useState();
     const [searchParams] = useSearchParams();
     const pageNumber = searchParams.get('page');
 
@@ -12,13 +14,42 @@ export function PostListPage() {
             const url = `http://localhost:3001/posts?${pageNumber ? `page=${pageNumber}` : "" }&pageSize=10`;
             fetch(url)
                 .then(response => response.json())
-                .then(postListJson => setPostList(postListJson.results));
+                .then(postListJson => {
+                    setPostList(postListJson.results);
+                    setNext(postListJson.next);
+                    setPrevious(postListJson.previous);
+                    });
         },
         [pageNumber]
     );
 
+    const nextPrevLinks = <div> 
+        {
+            previous ? <Link to={previous}>Previous</Link> : <></>
+        }
+        {
+            next ? <Link to={next}>Next</Link> : <></>
+        }
+
+    </div>
+
+    let postPage;
+
+    if (postList !== undefined) {
+        postPage = 
+            <>
+                {nextPrevLinks}
+                <PostList postlist={postList}/>
+                {nextPrevLinks}
+            </>
+    } else {
+        postPage = <p>Loading...</p>
+    }
+
     return (
-        postList !== undefined ? <PostList postlist={postList}/> : <p>Loading...</p>
+        <main>
+            {postPage}
+        </main>
     )
     
 }
