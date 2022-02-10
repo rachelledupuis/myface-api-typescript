a014495e4b890d40adaaa2016fd88dadcd7bb04d
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-02-10 14:34:15 +0000

    Added next and previous links to user list page - not working

--- public/src/myface/src/Components/Users/userListPage/UserListPage.jsx
+++ public/src/myface/src/Components/Users/userListPage/UserListPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { UserList } from "../userList/UserList";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 
 export function UserListPage() {
-    const[userList, setUserList] = useState()
+    const[userList, setUserList] = useState();
+    const[next, setNext] = useState();
+    const[previous, setPrevious] = useState();
     const [searchParams] = useSearchParams();
     const pageNumber = searchParams.get('page');
 
@@ -12,13 +14,42 @@ export function UserListPage() {
             const url = `http://localhost:3001/users?${pageNumber ? `page=${pageNumber}` : ""}&pageSize=10`;
             fetch(url)
                 .then(response => response.json())
-                .then(userListJson => setUserList(userListJson.results));
+                .then(userListJson => {
+                    setUserList(userListJson.results);
+                    setNext(userListJson.next);
+                    setPrevious(userListJson.previous);
+                });
         },
         [pageNumber]
     );
 
+    const nextPrevLinks = <div> 
+        {
+            previous ? <Link to={previous}>Previous</Link> : <></>
+        }
+        {
+            next ? <Link to={next}>Next</Link> : <></>
+        }
+
+    </div>
+
+    let userPage;
+
+    if (userPage !== undefined) {
+        userPage = 
+        <>
+           {nextPrevLinks}
+           <UserList userlist={userList}/>
+           {nextPrevLinks}
+        </>
+    } else {
+        userPage = <p>Loading...</p>
+    }
+
     return (
-        userList !== undefined ? <UserList userlist={userList}/> : <p>Loading...</p>
+        <main>
+            {userPage}
+        </main>
     )
     
 }
